c34d2cf853a923467d002a3c34a9cb23e3326e71
billymwangi <billy.mwangi@student.moringaschool.com>
2022-06-06 18:34:55 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if(ride <= 400) {
+    return `free sample`
+  }else if (ride > 2000){
+    return `30 dollars`
+  }else if (ride > 2500){
+    return `don\`t allow`
+  }
+
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  city === NYC?"Ok, sounds good":"No go";
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch(Tip) {
+    case 'Tip is generous':
+      return 'Thank you so much'
+      break;
+    case 'Tip is not generous':
+      return 'Thank you' 
+      break;
+    default: 
+      return 'Bye'
+  }
 }
+
